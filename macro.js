ba790e75452b876d99bf1a1cfab358d44ff47cf2
胡本绿 <benlv.hu@dianrong.com>
2019-05-30 16:54:04 +0800

    import 路径名唯一化

--- macro.js
+++ macro.js
@@ -10,77 +10,77 @@ const pages = {};
 
 // Remove not exist files, etc Rename, Delete
 function extractMeta(filePath, meta, config) {
-    Object.keys(pages).filter(filePath => {
-        try {
-            return !fs.existsSync(filePath)
-        } catch (e) {
-            return true;
-        }
-    }).forEach(notExistPage => {
-        delete pages[notExistPage];
-    });
+  Object.keys(pages).filter(filePath => {
+    try {
+      return !fs.existsSync(filePath)
+    } catch (e) {
+      return true;
+    }
+  }).forEach(notExistPage => {
+    delete pages[notExistPage];
+  });
 
-    pages[filePath] = meta;
-    config(pages);
+  pages[filePath] = meta;
+  config(pages);
 }
 
 module.exports = createMacro(webpackCommentImportMacros, { configName });
 
 function webpackCommentImportMacros({ references, state, babel, config }) {
 
-    if (!config) {
-        return;
-    }
-    if (typeof config !== 'function') {
-        throw new MacroError(`请指定函数类型，如：
+  if (!config) {
+    return;
+  }
+  if (typeof config !== 'function') {
+    throw new MacroError(`请指定函数类型，如：
     module.exports = {
         '${configName}': function(){}
     }
         `);
-    }
-    references.default &&
-        references.default.map(referencePath => {
-            if (referencePath.parentPath.type === 'CallExpression') {
-                requireWebpackCommentImport({ referencePath, state, babel, config });
-            } else {
-                throw new MacroError(
-                    `This is not supported: \`${referencePath
-                        .findParent(babel.types.isExpression)
-                        .getSource()}\`. Please see the ${configName} documentation`
-                );
-            }
-        });
+  }
+  references.default &&
+    references.default.map(referencePath => {
+      if (referencePath.parentPath.type === 'CallExpression') {
+        requireWebpackCommentImport({ referencePath, state, babel, config });
+      } else {
+        throw new MacroError(
+          `This is not supported: \`${referencePath
+            .findParent(babel.types.isExpression)
+            .getSource()}\`. Please see the ${configName} documentation`
+        );
+      }
+    });
 }
 
 function requireWebpackCommentImport({ referencePath, state, babel, config }) {
-    const callExpressionPath = referencePath.parentPath;
+  const callExpressionPath = referencePath.parentPath;
 
-    const arguments = callExpressionPath.get('arguments');
+  const arguments = callExpressionPath.get('arguments');
 
-    const argumentsValues = arguments.map((argument) => {
-        if (!argument) {
-            return undefined;
-        }
-        const code = argument.getSource();
-        let value;
-        try {
-            value = argument.evaluate().value
-        } catch (err) {
-            // swallow error, print better error below
-        }
+  const argumentsValues = arguments.map((argument) => {
+    if (!argument) {
+      return undefined;
+    }
+    const code = argument.getSource();
+    let value;
+    try {
+      value = argument.evaluate().value
+    } catch (err) {
+      // swallow error, print better error below
+    }
 
-        if (code && (value === undefined)) {
-            throw new MacroError(
-                `There was a problem evaluating the value of the argument for the code: ${code}. ` +
-                `If the value is dynamic, please make sure that its value is statically deterministic.`
-            );
-        }
-        return value;
-    })
+    if (code && (value === undefined)) {
+      throw new MacroError(
+        `There was a problem evaluating the value of the argument for the code: ${code}. ` +
+        `If the value is dynamic, please make sure that its value is statically deterministic.`
+      );
+    }
+    return value;
+  })
 
-    extractMeta(state.filename, {
-        callee: callExpressionPath.node.callee.name,
-        params: argumentsValues
-    }, config);
-    referencePath.parentPath.parentPath.remove();
+  extractMeta(state.filename, {
+    callee: callExpressionPath.node.callee.name,
+    params: argumentsValues
+  }, config);
+  referencePath.parentPath.parentPath.remove();
 }
