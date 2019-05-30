642eee701e88f36771a0d820726b4116086bf3d7
胡本绿 <benlv.hu@dianrong.com>
2019-05-30 16:49:50 +0800

    update usage

--- example/router-usage/src/entry/meta.js
+++ example/router-usage/src/entry/meta.js
@@ -1,4 +1,8 @@
 // Used for Generate src/router.js and src/router-config.json
 
-const components = require.context('~/pages/compile/', true, /\.js$/);
-components.keys().forEach(key => components(key));
+// const components = require.context('~/pages/compile/', true, /\.js$/);
+// components.keys().forEach(key => components(key));
+
+import 'meta.macro/example/router-usage/src/pages/compile/Detail2Page';
+import '~/pages/compile/HomePage';
+// import "~/pages/compile/LoginPage";
--- example/router-usage/src/pages/compile/Detail2Page.js
+++ example/router-usage/src/pages/compile/Detail2Page.js
@@ -3,7 +3,7 @@ import Controller from 'meta.macro';
 
 // 详情页异步加载
 @Controller('/detail', {
-  async: true,
+  async: false,
   chunk: 'common'
 })
 export default class extends React.Component {
